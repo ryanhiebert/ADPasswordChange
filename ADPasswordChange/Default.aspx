a02174a21b44bc9387d1bcea23811cdb39af8254
Ryan Hiebert <ryan@ryanhiebert.com>
2012-09-19 02:00:06 -0700

    Add Default.aspx, a very simple and unstyled password reset form

---
+++ ADPasswordChange/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ADPasswordChange.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Reset Your Password</title>
+</head>
+<body>
+    <asp:Label runat="server" ID="servername"></asp:Label>
+    <asp:Panel ID="form" runat="server" Width="500px">
+    <form id="form2" runat="server" action="" method="post">
+    <div>
+        <table border="0">
+            <tbody>
+                <tr>
+                    <td>Username:</td>
+                    <td><input id="loginname" name="loginname" type="text" /></td>
+                </tr>
+                <tr>
+                    <td>Old Password:</td>
+                    <td><input id="oldpass" name="oldpass" type="password" /></td>
+                </tr>
+                <tr>
+                    <td>New Password:</td>
+                    <td><input id="newpass" name="newpass" type="password" /></td>
+                </tr>
+                <tr>
+                    <td>Confirm New Password:</td>
+                    <td><input id="confpass" name="confpass" type="password" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input id="submit" type="submit" value="submit" /></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </form>
+    </asp:Panel>
+    <asp:Panel ID="status" runat="server" Width="500px">
+        <asp:Label ID="msg" runat="server"></asp:Label>
+    </asp:Panel>
+</body>
+</html>
---
+++ ADPasswordChange/Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.DirectoryServices;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADPasswordChange
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                form.Visible = false;
+                status.Visible = true;
+
+                string username = Request.Form["loginname"];
+                string oldpass = Request.Form["oldpass"];
+                string newpass = Request.Form["newpass"];
+                string confpass = Request.Form["confpass"];
+
+
+
+                try
+                {
+                    if (confpass.Equals(newpass) && !oldpass.Equals(newpass) && !string.Empty.Equals(newpass))
+                    {
+                        ResetPassword(username, oldpass, newpass);
+                        msg.Text = "Your password was successfully changed.";
+                    }
+                    else
+                    {
+                        msg.Text = "New Password invalid. It cannot be blank, or the same as your old password.";
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    msg.Text = "Unknown Username.";
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    msg.Text = "Invalid Username or Password.";
+                }
+                catch (Exception)
+                {
+                    msg.Text = "Something went wrong. Please Try Again. If this persists, notify your Administrator.";
+                }
+
+            }
+            else
+            {
+                form.Visible = true;
+                status.Visible = false;
+            }
+        }
+
+        protected void ResetPassword(string username, string password, string newpassword)
+        {
+            // Use the current comptuer's domain to contruct the LDAP connection string.
+            string dnsdomain = IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            List<string> parts = new List<string>();
+            foreach (string s in dnsdomain.Split('.'))
+            { 
+                parts.Add("dc=" + s);
+            }
+            string connectionString = "LDAP://" + dnsdomain + "/" + string.Join(",", parts);
+
+            DirectoryEntry me = null;
+
+            try
+            {
+                using (DirectoryEntry searchRoot = new DirectoryEntry(connectionString, username, password, AuthenticationTypes.Secure))
+                {
+                    using (DirectorySearcher ds = new DirectorySearcher(searchRoot, "(sAMAccountName=" + username + ")"))
+                    {
+                        SearchResult sr = ds.FindOne();
+                        if (sr != null)
+                            me = sr.GetDirectoryEntry();
+                        else
+                            throw new ArgumentException("Failed to find login name!");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw new UnauthorizedAccessException("Invalid username or password!");
+            }
+
+            me.Invoke("SetPassword", new object[] { newpassword });
+            me.CommitChanges();
+        }
+    }
+}
